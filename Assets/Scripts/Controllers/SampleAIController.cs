68cca310032008796f946f11373c3eeaa294051a
Joshua Thornton <josthorn@uat.edu>
2020-11-24 11:51:59 -0500

    Waypoint tank complete with its own materials. Tipping problem solved by ensuring waypoint elevations match the tank's elevation.

--- Assets/Scripts/Controllers/SampleAIController.cs
+++ Assets/Scripts/Controllers/SampleAIController.cs
@@ -47,6 +47,10 @@ public class SampleAIController : MonoBehaviour {
     // Used only for the PingPong behavior.
     private bool isGoingForward = true;
 
+    // Whether or not the tank has reached the end of the waypoint system.
+    // Used only for the Stop behavior.
+    private bool isStopped = false;
+
     #endregion Fields
 
     #region Unity Methods
@@ -91,13 +95,29 @@ public class SampleAIController : MonoBehaviour {
             // Turn off this script to prevent errors.
             this.enabled = false;   
         }
+        // Else, the wapoints array is not empty.
+        else
+        {
+            // If the elevation for the next waypoint does not match the tank's elevation,
+            if (waypoints[currentWaypoint].transform.position.y != tf.position.y)
+            {
+                // then level out all of the waypoints' elevations.
+                LevelOutWaypoints();
+            }
+        }
     }
 
     // Called every frame.
     void Update()
     {
-        // If we can rotate towards the current waypoint (done during the if call),
-        if (motor.RotateTowards(waypoints[currentWaypoint].position, data.turnSpeed))
+        // If stopped (meaning Tank is set to Stop behavior and has reached the end of the waypoint system),
+        if (isStopped)
+        {
+            // Do nothing.
+            // This prevents tank from turning constantly while standing on the final waypoint.
+        }
+        // Else, if we can rotate towards the current waypoint (done during the if call),
+        else if (motor.RotateTowards(waypoints[currentWaypoint].position, data.turnSpeed))
         {
             // then the tank was able to rotate towards the waypoint this frame.
             // Do nothing.
@@ -113,93 +133,139 @@ public class SampleAIController : MonoBehaviour {
         if (Vector3.SqrMagnitude(waypoints[currentWaypoint].position - tf.position) < (closeEnough_Squared))
         {
             // then advance to the next waypoint according to the chosen behavior.
-            NextWaypoint();
+            NextWaypoint_Dispatch();
         }
     }
+    #endregion Unity Methods
 
-    // Chooses the nexy waypoint to head towards based on the chosen behavior (loopType).
-    private void NextWaypoint()
+    #region Dev-Defined Methods
+    // Chooses the next waypoint to head towards based on the chosen behavior (loopType).
+    private void NextWaypoint_Dispatch()
     {
         // Based on the chosen loop type, perform a certain action.
         switch (loopType)
         {
             // If set to Stop,
             case LoopType.Stop:
-                // and if NOT already at the end,
-                if (currentWaypoint < waypoints.Length - 1)
-                {
-                    // then add 1 to go tot he next waypoint in the list.
-                    currentWaypoint++;
-                }
-                // Else, already at the end.
-                else
-                {
-                    // Do nothing.
-                }
+                // then perform the "next waypoint" actions for the LoopType.Stop behavior.
+                NextWaypoint_Stop();
                 break;
 
             // If set to Loop,
             case LoopType.Loop:
-                // If NOT already at the end,
-                if (currentWaypoint < waypoints.Length - 1)
-                {
-                    // then add 1 to the currentWaypoint.
-                    currentWaypoint++;
-                }
-                // Else, already at the end.
-                else
-                {
-                    // Reset the current waypoint index to 0.
-                    currentWaypoint = 0;
-                }
+                // then perform the "next waypoint" actions for the LoopType.Loop behavior.
+                NextWaypoint_Loop();
                 break;
 
             // If set to PingPong,
             case LoopType.PingPong:
-                // If going forward,
-                if (isGoingForward)
-                {
-                    // and if NOT already at the end,
-                    if (currentWaypoint < waypoints.Length - 1)
-                    {
-                        // then add 1 to the currentWaypoint.
-                        currentWaypoint++;
-                    }
-                    // Else, already at the end (going forward).
-                    else
-                    {
-                        // Subtract 1 from the currentWaypoint.
-                        currentWaypoint--;
+                // then perform the "next waypoint" actions for the LoopType.PingPong behavior.
+                NextWaypoint_PingPong();
+                break;
+        } // currentWaypoint has been set.
 
-                        // Flip the direction.
-                        isGoingForward = false;
-                    }
-                }
-                // Else, tank is currently heading backwards through the waypoints.
-                else
-                {
-                    // If NOT already at the beginning,
-                    if (currentWaypoint > 0)
-                    {
-                        // then subtract 1 from currenWaypoint.
-                        currentWaypoint--;
-                    }
-                    // Else, already back to the beginning of the array.
-                    else
-                    {
-                        // Add 1 to the index.
-                        currentWaypoint++;
-
-                        // Flip the direction.
-                        isGoingForward = true;
-                    }
-                }
-                    break;
+        // If the elevation for the next waypoint does not match the tank's elevation,
+        if (waypoints[currentWaypoint].transform.position.y != tf.position.y)
+        {
+            // then level out all of the waypoints' elevations.
+            LevelOutWaypoints();
         }
     }
-    #endregion Unity Methods
 
-    #region Dev-Defined Methods
+    // Performs the "next waypoint" actions for the LoopType.Stop behavior.
+    private void NextWaypoint_Stop()
+    {
+        // If NOT already at the end,
+        if (currentWaypoint < waypoints.Length - 1)
+        {
+            // then add 1 to go tot he next waypoint in the list.
+            currentWaypoint++;
+        }
+        // Else, already at the end.
+        else
+        {
+            // Tank should not stop.
+            isStopped = true;
+        }
+    }
 
+    // Performs the "next waypoint" actions for the LoopType.Loop behavior.
+    private void NextWaypoint_Loop()
+    {
+        // If NOT already at the end,
+        if (currentWaypoint < waypoints.Length - 1)
+        {
+            // then add 1 to the currentWaypoint.
+            currentWaypoint++;
+        }
+        // Else, already at the end.
+        else
+        {
+            // Reset the current waypoint index to 0.
+            currentWaypoint = 0;
+        }
+    }
+
+    // Performs the "next waypoint" actions for the LoopType.PingPong behavior.
+    private void NextWaypoint_PingPong()
+    {
+        // If going forward,
+        if (isGoingForward)
+        {
+            // and if NOT already at the end,
+            if (currentWaypoint < waypoints.Length - 1)
+            {
+                // then add 1 to the currentWaypoint.
+                currentWaypoint++;
+            }
+            // Else, already at the end (going forward).
+            else
+            {
+                // Subtract 1 from the currentWaypoint.
+                currentWaypoint--;
+
+                // Flip the direction.
+                isGoingForward = false;
+            }
+        }
+        // Else, tank is currently heading backwards through the waypoints.
+        else
+        {
+            // If NOT already at the beginning,
+            if (currentWaypoint > 0)
+            {
+                // then subtract 1 from currenWaypoint.
+                currentWaypoint--;
+            }
+            // Else, already back to the beginning of the array.
+            else
+            {
+                // Add 1 to the index.
+                currentWaypoint++;
+
+                // Flip the direction.
+                isGoingForward = true;
+            }
+        }
+    }
+
+    // Sets all of the appropriate waypoints to the current elevation.
+    private void LevelOutWaypoints()
+    {
+        // Iterate through the waypoints array.
+        foreach (Transform waypoint in waypoints)
+        {
+            // Set each transform's y equal to this tank's y.
+            // This prevents the tank from attempting to move into the ground or above it.
+            // First, get the position of the waypoint.
+            Vector3 p = waypoint.position;
+
+            // Change the y to match.
+            p.y = tf.position.y;
+
+            // Set the waypoint's position to the modified value.
+            waypoint.position = p;
+        }
+    }
     #endregion Dev-Defined Methods
 }
